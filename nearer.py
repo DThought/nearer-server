ad818efbaefa5bbcdf038809a8d9d3acda43fc78
Deep Toaster <ti.42.plus@gmail.com>
2016-01-09 19:16:39 -0800

    initial commit

---
+++ nearer.py
@@ -0,0 +1,133 @@
+import base64
+from collections import deque
+import os
+import queue
+import re
+import socket
+import sys
+import threading
+import time
+
+
+class NearerCommand(object):
+    PLAY, SKIP, STOP, APPEND, REMOVE = range(5)
+
+    def __init__(self, type, data=None):
+        self.type = type
+        self.data = data
+
+
+class NearerData(object):
+    def __init__(self, uri, length):
+        self.uri = uri
+        self.length = length
+
+
+class NearerThread(threading.Thread):
+    def __init__(self, out = None, recv=None):
+        super(NearerThread, self).__init__()
+        self.out = out or sys.stdout
+        self.recv = recv or queue.Queue()
+        self.alive = threading.Event()
+        self.alive.set()
+        self._handlers = {
+            NearerCommand.PLAY: self._nplay,
+            NearerCommand.SKIP: self._nskip,
+            NearerCommand.STOP: self._nstop,
+            NearerCommand.APPEND: self._nappend,
+            NearerCommand.REMOVE: self._nremove
+        }
+        self._playlist = deque()
+        self._playlist.append(None)
+        self._nplaying = False
+        self._next = None
+
+    def run(self):
+        while self.alive.is_set():
+            try:
+                cmd = self.recv.get(True, 1)
+                self._handlers[cmd.type](cmd.data)
+                if self._nplaying:
+                    if self._next < time.time() and self._playlist:
+                        self._playlist.popleft()
+                        if self._playlist:
+                            self._write('playurl ' + self._playlist[0].uri)
+                            self._next = time.time();
+                            self._next += int(self._playlist[0].length)
+                        else:
+                            self._nplaying = False
+            except queue.Empty as e:
+                continue
+
+    def join(self, timeout=None):
+        self.alive.clear()
+        self._nstop(None)
+        threading.Thread.join(self, timeout)
+
+    def _nplay(self, data):
+        if self._nplaying:
+            return
+        self._nplaying = True
+        self._nskip(data)
+
+    def _nskip(self, data):
+        self._next = time.time()
+
+    def _nstop(self, data):
+        self._nplaying = False
+        self._write('quit')
+
+    def _nappend(self, data):
+        self._playlist.append(data)
+
+    def _nremove(self, data):
+        dirty = True
+        while dirty:
+            dirty = False
+            for track in self._playlist:
+                if track.uri == data.uri:
+                    dirty = True
+                    self._playlist.remove(track)
+                    break
+
+    def _write(self, string):
+        with open(out, 'w') as f:
+            f.write(string)
+
+
+srv = sys.argv[1]
+out = sys.argv[2]
+
+try:
+    os.unlink(srv)
+except FileNotFoundError:
+    pass
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+print('starting up on %s' % srv, file=sys.stderr)
+sock.bind(srv)
+sock.listen(1)
+thread = NearerThread(out)
+thread.start()
+while True:
+    conn, address = sock.accept()
+    try:
+        data = conn.recv(64)
+        if data:
+            data = data.decode('ascii').split()
+            try:
+                type = getattr(NearerCommand, data[0])
+                if len(data) == 3:
+                    if re.search(r'/[^a-zA-Z0-9_-]/', data[1]):
+                        continue
+                    if re.search(r'/\S/', data[2]):
+                        continue
+                    cmd = NearerCommand(type, NearerData(data[1], data[2]))
+                else:
+                    cmd = NearerCommand(type)
+                thread.recv.put(cmd)
+                print(data)
+            except AttributeError:
+                continue
+    finally:
+        conn.close()
